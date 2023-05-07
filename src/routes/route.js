fcdd4b6f034fb95896aca782e760377c244fc82b
Rahulfunctionup <rahulkeshari801@gmail.com>
2023-05-07 22:49:10 +0530

    rahul

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const flightController = require('../controllers/flightController');
-router.get('/api' , (req,res) => {
-    res.send('first api')
+router.get('/' , (req,res) => {
+    res.send('<h1>Hello From Express App </h1>')
 });
 router.post('/flight', flightController.createFlight);
 router.get('/flights',flightController.getFlight)
